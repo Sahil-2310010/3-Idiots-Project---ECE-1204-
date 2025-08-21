e4995af27f1304859d2e4679a4c7503127e14ab7
Zawad2310018 <zawad8790@gmail.com>
2025-08-21 18:25:37 +0600

    Update zawad.cpp

--- The Project/zawad.cpp
+++ The Project/zawad.cpp
@@ -1,10 +1,44 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
+int balance = 1000;
 
+template<class T>
+class AddBalance{
+ T amount;
 
-int main()
-{
- 
-  
-  return 0;
+ public:
+     AddBalance(T amnt)
+     {
+         try{
+         if(amnt<0)
+         {
+             throw "Invalid Amount";
+         }
+         else
+         {
+             amount =amnt;
+         }
+         }
+         catch(const char*e)
+         {
+             cout<<e<<endl;
+             amount = 0;
+         }
+
+     }
+
+     T getBalance()
+     {
+         return amount+balance;
+     }
+};
+
+int main() {
+    /*AddBalance<int> a1(500);
+    cout << "Balance: " << a1.getBalance() << endl;
+
+    AddBalance<int> a2(-100); // Will trigger exception
+    cout << "Balance: " << a2.getBalance() << endl;
+    */
+    return 0;
 }
